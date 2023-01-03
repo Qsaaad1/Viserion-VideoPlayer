f5e0ea90c0439de1b46b19044335bbadeeeae322
Saad Ahmed Qureshi <101616957+Qsaaad1@users.noreply.github.com>
2023-01-03 19:43:53 +0530

    Update script.js

--- Viserion-VideoPlayer/script.js
+++ Viserion-VideoPlayer/script.js
@@ -8,16 +8,62 @@ const video = document.querySelector("video")
 
 
 document.addEventListener("keydown", e => {
-    switch (e.key) {
+    switch (e.key.toLowerCase()) {
         case " ":
             togglePlay()
+            break
+            case "f":
+                toggleFullScreenMode()
+                break
+                case "i":
+                    toggleMiniPlayerMode()
+                    break    
+                    case "t":
+                        toggleTheaterMode()
+                        break
     }
 })
 
 // View Modes
 
+theaterBtn.addEventListener("click", toggleTheaterMode)
+fullScreenBtn.addEventListener("click", toggleFullScreenMode)
+miniPlayerBtn.addEventListener("click", toggleMiniPlayerMode)
 
+function toggleTheaterMode() {
+    videoContainer.classList.toggle("theater")
+}
+
+function toggleFullScreenMode() {
+    if (document.webkitFullscreenElement == null) {
+      videoContainer.webkitRequestFullscreen()
+    } else {
+      document.webkitExitFullscreen()
+    }
+  }
+  
+  function toggleMiniPlayerMode() {
+    if (videoContainer.classList.contains("mini-player")) {
+      document.exitPictureInPicture()
+    } else {
+      video.requestPictureInPicture()
+    }
+  }
+  
+  document.addEventListener("fullscreenchange", () => {
+    videoContainer.classList.toggle("full-screen", document.webkitFullscreenElement)  // REMOVE IT LATER
+  })
+  
+  video.addEventListener("enterpictureinpicture", () => {
+    videoContainer.classList.add("mini-player")
+  })
+  
+  video.addEventListener("leavepictureinpicture", () => {
+    videoContainer.classList.remove("mini-player")
+  })
+  
 // Play/Pause
+
 playPauseBtn.addEventListener('click', togglePlay)
 video.addEventListener("click", togglePlay)
 
