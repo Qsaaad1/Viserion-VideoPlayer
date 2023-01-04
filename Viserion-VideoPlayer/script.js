c3ff4083ac88ccf3dd77232b2fa55ab035a34637
Saad Ahmed Qureshi <101616957+Qsaaad1@users.noreply.github.com>
2023-01-04 19:12:05 +0530

    Update script.js

--- Viserion-VideoPlayer/script.js
+++ Viserion-VideoPlayer/script.js
@@ -2,6 +2,10 @@ const playPauseBtn = document.querySelector(".play-pause-btn")
 const theaterBtn = document.querySelector(".theater-btn")
 const fullScreenBtn = document.querySelector(".full-screen-btn")
 const miniPlayerBtn = document.querySelector(".mini-player-btn")
+const muteBtn = document.querySelector(".mute-btn")
+const currentTimeElem = document.querySelector(".current-time")
+const totalTimeElem = document.querySelector(".total-time")
+const volumeSlider = document.querySelector(".volume-slider")
 const videoContainer = document.querySelector(".video-container")
 const video = document.querySelector("video")
 
@@ -21,7 +25,75 @@ document.addEventListener("keydown", e => {
                     case "t":
                         toggleTheaterMode()
                         break
+                        case "m":
+                            toggleMute()
+                            break
+                            case "arrowleft":
+                              case "j":
+                                skip(-5)
+                                break
+                              case "arrowright":
+                              case "l":
+                                skip(5)
+                                break
     }
+})
+
+// Duration
+
+video.addEventListener("loadeddata", () => {
+  totalTimeElem.textContent = formatDuration(video.duration)
+})
+
+video.addEventListener("timeupdate", () => {
+  currentTimeElem.textContent = formatDuration(video.currentTime)
+})
+
+const leadingZeroFormatter = new Intl.NumberFormat(undefined, {
+  minimumIntegerDigits: 2
+})
+function formatDuration(time) {
+  const seconds = Math.floor(time % 60)
+  const minutes = Math.floor(time / 60) % 60
+  const Hours = Math.floor(time / 3600)
+  if (Hours === 0) {
+    return `${minutes}:${leadingZeroFormatter.format(seconds)}`
+  } else {
+    return `${hours}:${leadingZeroFormatter.format(minutes)}:${leadingZeroFormatter.format(seconds)}`
+
+  }
+}
+
+function skip(duration) {
+  video.currentTime += duration
+  video.play()
+}
+
+// V0lume
+
+muteBtn.addEventListener("click", toggleMute)
+volumeSlider.addEventListener("input", e => {
+  video.volume = e.target.value
+  video.muted = e.target.value === 0
+})
+
+function toggleMute() {
+  video.muted = !video.muted
+}
+
+video.addEventListener("volumechange", () => {
+  volumeSlider.value = video.volume
+  let volumeLevel
+  if (video.muted || video.volume === 0) {
+    volumeSlider.value = 0
+    volumeLevel = "muted"
+  } else if (video.volume >= 0.5) {
+    volumeLevel = "high"
+  } else {
+    volumeLevel = "low"
+  }
+
+  videoContainer.dataset.volumeLevel = volumeLevel
 })
 
 // View Modes
